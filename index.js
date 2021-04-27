a42f941f1d51c2c4245983833dd09a76b1b2ba6f
Felipe Larraguibel <felipelarra.araya@gmail.com>
2021-04-27 11:37:35 -0300

    First commit - configuración inicial, creación de controlador y rutas

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const express = require('express');
+const cors = require('cors');
+require ('dotenv').config();
+
+
+const app = express();
+
+//Directorio publico
+app.use( express.static('public')); 
+
+//Cors
+app.use( cors() );
+
+//Lectura y parseo del body
+app.use( express.json() );
+
+//Rutas
+app.use( '/api/auth', require('./routes/auth') );
+
+
+
+app.listen( process.env.PORT , () => {
+    console.log( `Servidor corriendo en puerto ${ process.env.PORT }`);
+});
