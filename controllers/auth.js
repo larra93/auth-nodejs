050eb0d2c259d7c419702ef65fe9b902a7fa6367
Felipe Larraguibel <felipelarra.araya@gmail.com>
2021-04-27 16:08:33 -0300

    implementación de middleware para validar campos

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,18 +2,20 @@ const { response } = require('express');
 
 const crearUsuario = (req, res = response) => {
 
+  
     const { email, name, password } = req.body;
-    console.log( email, name, password );
+    
     return res.json({
         ok:true,
         msg:'Crear usuario/new'
     });
 }
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req, res = response) => {
 
+   
     const { email, password } = req.body;
-    console.log( email , password );
+    
 
     return res.json({
         ok:true,
